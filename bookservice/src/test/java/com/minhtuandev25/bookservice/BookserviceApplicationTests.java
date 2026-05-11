41b2b04479acdc07db84065e33209ab7e987e8a9
MinhTuanDev25 <ntmt2520job@gmail.com>
2026-05-11 15:01:50 +0700

    Initial commit

---
+++ bookservice/src/test/java/com/minhtuandev25/bookservice/BookserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhtuandev25.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
