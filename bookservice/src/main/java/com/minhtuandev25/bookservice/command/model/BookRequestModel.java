ab70226c7493b23958a9f21846cd8f47b13f428c
MinhTuanDev25 <ntmt2502job@gmail.com>
2026-05-14 09:48:53 +0700

    add validation and handle exception for validation

--- bookservice/src/main/java/com/minhtuandev25/bookservice/command/model/BookRequestModel.java
+++ bookservice/src/main/java/com/minhtuandev25/bookservice/command/model/BookRequestModel.java
@@ -1,6 +1,11 @@
 package com.minhtuandev25.bookservice.command.model;
 
-import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -9,8 +14,11 @@ import lombok.*;
 public class BookRequestModel {
     private String id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 30, message = "Name must be between 3 and 30 characters")
     private String name;
 
+    @NotBlank(message = "Author is required")
     private String author;
 
     private Boolean isReady;
